e3e8767634a935ad5bcf798cd659aa9391671971
Praveen Yengilichetty <prvnsms@gmail.com>
2019-11-30 18:29:52 +0530

    Added allowed hosts 2

--- mistay/settings.py
+++ mistay/settings.py
@@ -137,7 +137,7 @@ STATICFILES_DIRS = (
 )
 
 #  Add configuration for static files storage using whitenoise
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.django.CompressedManifestStaticFilesStorage'
 
 import dj_database_url
 
